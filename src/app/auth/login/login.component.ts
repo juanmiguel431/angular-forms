8d6d70fd53a328a07c081d0d933c73bb2a78f99e
Juan Miguel Paulino Carpio <juanmiguel431@gmail.com>
2026-07-04 22:33:23 -0400

    Enable form handling in LoginComponent with Angular FormsModule integration

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
+  imports: [FormsModule],
 })
-export class LoginComponent {}
+export class LoginComponent {
+
+  protected onSubmit(form: NgForm) {
+    console.log(form);
+    console.log(form.value);
+  }
+}
