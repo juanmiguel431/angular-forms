59b50f348b6c290513d29a8c6a2c8a7a709b8509
Juan Miguel Paulino Carpio <juanmiguel431@gmail.com>
2026-07-04 22:13:34 -0400

    Base code

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [LoginComponent],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('angular-forms');
+
 }
