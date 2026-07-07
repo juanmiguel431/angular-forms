57a6af35c411b45314a9a59e6c8538df641c43b8
Juan Miguel Paulino Carpio <juanmiguel431@gmail.com>
2026-07-06 22:20:15 -0400

    Refactor validators to prepare for async implementation of email uniqueness check

--- src/app/validators/index.ts
+++ src/app/validators/index.ts
@@ -1,5 +1,5 @@
 import { AbstractControl } from '@angular/forms';
-import { of } from 'rxjs';
+import { delay, map, of } from 'rxjs';
 
 export function mustContainQuestionMarkValidator(control: AbstractControl) {
   if (control.value.includes('?')) {
@@ -10,10 +10,32 @@ export function mustContainQuestionMarkValidator(control: AbstractControl) {
 }
 
 export function emailIsUniqueValidator(control: AbstractControl) {
-
   if (control.value !== 'test@test.com') {
     return of(null);
   }
 
   return of({ notUnique: true });
 }
+
+// export async function emailIsUniqueValidator(control: AbstractControl) {
+//   await new Promise((r) => setTimeout(r, 3000));
+//
+//   if (control.value !== 'test@test.com') {
+//     return null;
+//   }
+//
+//   return { notUnique: true };
+// }
+
+// export function emailIsUniqueValidator(control: AbstractControl) {
+//   return of(control.value).pipe(
+//     delay(3000),
+//     map((value) => {
+//       if (value !== 'test@test.com') {
+//         return null;
+//       }
+//
+//       return { notUnique: true };
+//     }),
+//   );
+// }
