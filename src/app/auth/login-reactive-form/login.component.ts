62cd634fd9a6aa5d305834c8073a91b2e1405a8d
Juan Miguel Paulino Carpio <juanmiguel431@gmail.com>
2026-07-06 21:39:11 -0400

    reactive-forms - Add validation checks

--- src/app/auth/login-reactive-form/login.component.ts
+++ src/app/auth/login-reactive-form/login.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -10,9 +10,35 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 })
 export class LoginComponent {
   protected form = new FormGroup({
-    email: new FormControl(''),
-    password: new FormControl(''),
+    email: new FormControl('', {
+      validators: [Validators.required, Validators.email],
+    }),
+    password: new FormControl('', {
+      validators: [Validators.required, Validators.minLength(6), Validators.maxLength(20)],
+    }),
   });
+
+  protected get formIsInvalid() {
+    return (
+      this.form.controls.email.touched && this.form.controls.password.touched && this.form.invalid
+    );
+  }
+
+  protected get emailIsInvalid() {
+    return (
+      this.form.controls.email.touched &&
+      this.form.controls.email.dirty &&
+      this.form.controls.email.invalid
+    );
+  }
+
+  protected get passwordIsInvalid() {
+    return (
+      this.form.controls.password.touched &&
+      this.form.controls.password.dirty &&
+      this.form.controls.password.invalid
+    );
+  }
 
   protected onSubmit() {
     console.log(this.form.value);
