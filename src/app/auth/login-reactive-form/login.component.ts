327737325b22457da1e6aaa38d8dd9ed182d2487
Juan Miguel Paulino Carpio <juanmiguel431@gmail.com>
2026-07-06 21:23:35 -0400

    reactive form implementation - link template with reactive form

--- src/app/auth/login-reactive-form/login.component.ts
+++ src/app/auth/login-reactive-form/login.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
-  imports: [],
+  imports: [ReactiveFormsModule],
 })
 export class LoginComponent {
+  protected form = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+  });
 
+  protected onSubmit() {
+    console.log(this.form.value);
+  }
 }
