3b1eb6828c134e8f704565a22ddf588b5bb58cb9
Juan Miguel Paulino Carpio <juanmiguel431@gmail.com>
2026-07-06 23:48:57 -0400

    reactive-forms - Implement SignupComponent with reactive form and basic validation

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { LoginComponent } from './auth/login-reactive-form/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
 
 @Component({
   selector: 'app-root',
-  imports: [LoginComponent],
+  imports: [LoginComponent, SignupComponent],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App {
-
-}
+export class App {}
--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,9 +1,23 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
-  standalone: true,
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css',
+  imports: [ReactiveFormsModule],
 })
-export class SignupComponent {}
+export class SignupComponent {
+  protected form = new FormGroup({
+    email: new FormControl('', {
+      validators: [Validators.email, Validators.required],
+    }),
+    password: new FormControl('', {
+      validators: [Validators.required, Validators.minLength(8)],
+    }),
+  });
+
+  protected onSubmitForm() {
+    console.log(this.form.value);
+  }
+}
