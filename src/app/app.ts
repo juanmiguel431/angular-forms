d93b8c3020fbb0ba49a427c6d8ad61dca9ba4c37
Juan Miguel Paulino Carpio <juanmiguel431@gmail.com>
2026-07-06 21:08:57 -0400

    Rename folder to reflect template-driven

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LoginComponent } from './auth/login/login.component';
+import { LoginComponent } from './auth/login-template-driven/login.component';
 
 @Component({
   selector: 'app-root',
