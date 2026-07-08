ebe3f72bc804a046b8456c27c857e534e9680eae
Juan Miguel Paulino Carpio <juanmiguel431@gmail.com>
2026-07-07 23:17:11 -0400

    reactive-forms - Bind template fields to reactive form controls in SignupComponent

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
+type Role = 'student' | 'teacher' | 'employee' | 'founder' | 'other';
+
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
@@ -15,6 +17,17 @@ export class SignupComponent {
     password: new FormControl('', {
       validators: [Validators.required, Validators.minLength(8)],
     }),
+    confirmPassword: new FormControl('', {
+      validators: [Validators.required, Validators.minLength(8)],
+    }),
+    firstName: new FormControl('', { validators: [Validators.required] }),
+    lastName: new FormControl('', { validators: [Validators.required] }),
+    street: new FormControl('', { validators: [Validators.required] }),
+    number: new FormControl('', { validators: [Validators.required] }),
+    postalCode: new FormControl('', { validators: [Validators.required] }),
+    city: new FormControl('', { validators: [Validators.required] }),
+    role: new FormControl<Role>('student', { validators: [Validators.required], }),
+    agree: new FormControl(false, { validators: [Validators.required], }),
   });
 
   protected onSubmitForm() {
